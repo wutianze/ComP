74af37a38dc3f67086d14908834b7dc70a5bb625
sooner <sooner1943@gmail.com>
2020-03-21 17:37:53 +0800

    nodelet ros

--- ros/build/CTestTestfile.cmake
+++ ros/build/CTestTestfile.cmake
@@ -6,3 +6,4 @@
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
 subdirs("beginner_tutorials")
+subdirs("example_pkg")
---
+++ ros/build/example_pkg/catkin_generated/pkg.installspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "${prefix}/include".split(';') if "${prefix}/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "nodelet;roscpp".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lexample_pkg".split(';') if "-lexample_pkg" != "" else []
+PROJECT_NAME = "example_pkg"
+PROJECT_SPACE_DIR = "/ros_test/AD_Middle_Test/ros/install"
+PROJECT_VERSION = "0.0.0"
