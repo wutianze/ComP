6976710b81e42ccc59610f690ac4767b7496d7ef
sauron <1369130123@qq.com>
2020-03-27 09:35:32 +0800

    timer cyber

---
+++ cyber/test_component/C.cc
@@ -0,0 +1,51 @@
+/******************************************************************************
+ * Copyright 2018 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+#include <memory>
+#include "cyber/component/component.h"
+#include "cyber/AD_Middle_Test/cyber/test.pb.h"
+#include "cyber/time/time.h"
+#include <fstream>
+
+using apollo::cyber::Component;
+using apollo::cyber::ComponentBase;
+using apollo::cyber::AD_Middle_Test::cyber::Bytes;
+using apollo::cyber::Time;
+class C:public Component<Bytes>{
+	private:
+		uint64_t count;
+  std::ofstream ofs;
+	public:
+	bool Init() {
+  AINFO << "C init";
+  count = 0;
+  ofs.open("/apollo/data/log/test/intra.txt",std::ios::trunc);
+  ofs.close();
+  return true;
+}
+bool Proc(const std::shared_ptr<Bytes>& msg0) {
+	uint64_t lan = Time::Now().ToNanosecond()-msg0->timestamp();
+  ofs.open("/apollo/data/log/test/intra.txt",std::ios::app);
+  ofs<<lan<<std::endl;
+  ofs.close();
+	count++;
+	AINFO<<"count now:"<<count;
+	AINFO<<"id:"<<msg0->id();
+	return true;
+}
+};
+CYBER_REGISTER_COMPONENT(C)
+
+
