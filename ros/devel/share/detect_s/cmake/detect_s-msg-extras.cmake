c5c06f600b08fafc5dd2e9695c90c9f4b647ad57
sooner <sooner1943@gmail.com>
2020-04-22 23:24:27 +0800

    part ros test

---
+++ ros/devel/share/detect_s/cmake/detect_s-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(detect_s_MESSAGE_FILES "/ros_test/AD_Middle_Test/ros/src/detect_s/msg/TestImage.msg")
+set(detect_s_SERVICE_FILES "")
---
+++ ros/devel/share/detect_s/cmake/detect_s-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(detect_s_MSG_INCLUDE_DIRS "/ros_test/AD_Middle_Test/ros/src/detect_s/msg")
+set(detect_s_MSG_DEPENDENCIES sensor_msgs;std_msgs)
