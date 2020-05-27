f15f4d45d17fe2bbabc4d42ad7a4d4b78de626b3
sauron <1369130123@qq.com>
2020-05-27 23:28:36 +0800

    1t1 test

--- cyber/simple_s/run_shm_t.sh
+++ cyber/simple_s/run_shm_t.sh
@@ -1,7 +1,7 @@
 # 1:size 2:number of subscribers 3: launch file name and conf file name(should be the same as process_name in launch file)
 # ex. ./run.sh 1 1 1t1
 kill -9 `ps -A|grep java |awk '{print $1}'` 
-log_dir="/apollo/data/log/test/1t1_50hz_4c_4t_ci"
+log_dir="/apollo/data/log/test/1t1_50hz_4c_4t_cs_t"
 mkdir $log_dir
 for ss in 1024 8192 16384 65536 262144 524288 1048576 2097152 4194304 8388608
 do
@@ -27,6 +27,6 @@ for f in $filesname
 do
 	echo $f
 	#cat /apollo/data/log/test/tmp/$f
-	mv /apollo/data/log/test/tmp/$f $log_dir/$ss"_1t1_50_4_4_ci.csv"
+	mv /apollo/data/log/test/tmp/$f $log_dir/$ss"_1t1_50_4_4_cs_t.csv"
 done
 done
