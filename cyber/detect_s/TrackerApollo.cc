91510edb22cb8f4b5cacf00b28b31d2687bceff0
sauron <1369130123@qq.com>
2020-12-08 15:34:51 +0800

    cyber new darknet

--- cyber/detect_s/TrackerApollo.cc
+++ cyber/detect_s/TrackerApollo.cc
@@ -39,6 +39,7 @@ class TrackerApollo:public Component<Frame,YoloResult>{
 		std::string fn;
 		vector<vector<uint64_t>>tra_latency;
 		vector<vector<uint64_t>>cal_latency;
+		CvTracker cvtra;
 
 		std::shared_ptr<Writer<TrackResult>> writer1 = nullptr;
 		~TrackerApollo(){
@@ -72,16 +73,17 @@ class TrackerApollo:public Component<Frame,YoloResult>{
 				cal_latency.push_back(tmp);
 			}
 			writer1 = node_->CreateWriter<TrackResult>("/"+fn);
-			tracker_create("KCF");
+			cvtra.tracker_create("KCF");
 			return true;
 		}
 		bool Proc(const std::shared_ptr<Frame>& msg0,const std::shared_ptr<YoloResult>& msg1) {
 			uint64_t receive_time = Time::Now().ToNanosecond();
-			//AINFO<<"tracker transfer time:"<<receive_time - msg0->timestamp();
+			AINFO<<"tracker transfer time:"<<receive_time - msg0->timestamp();
 			tra_latency[0].push_back(receive_time - msg0->timestamp());
 			tra_latency[1].push_back(receive_time - msg1->timestamp());
 			Mat m;
 			OcvMat content = msg0->mat();
+			AINFO<<" content rows:"<<content.rows()<<" cols:"<<content.cols();
 			m.create(content.rows(),
 					content.cols(),
 					content.elt_type());
@@ -92,6 +94,7 @@ class TrackerApollo:public Component<Frame,YoloResult>{
 						const_cast<char *>(content.mat_data().data()) + dataSize),
 					m.data);
 
+			AINFO<<"get data";
 			if(t_i){
 				t_i = false;
 				auto bboxSize = msg1->bboxresult_size();
@@ -100,18 +103,23 @@ class TrackerApollo:public Component<Frame,YoloResult>{
 					AINFO<<"random place";
 					resultbox = Rect2d(287, 23, 86, 320);
 				}else{
+
 					Bbox tmpBox = msg1->bboxresult(0);
+
+					AINFO<<"tmpBox.x:"<<tmpBox.x()<<" y:"<<tmpBox.y()<<" w:"<<tmpBox.w()<<" h:"<<tmpBox.h();
 					resultbox = Rect2d(double(tmpBox.x()),double(tmpBox.y()),double(tmpBox.w()),double(tmpBox.h()));
 				}
-				tracker_init(m,resultbox);
+					AINFO<<"init tracker";
+				cvtra.tracker_init(m,resultbox);
 			}else{
+					AINFO<<"tracker test";
 				uint64_t alog_start_time = Time::Now().ToNanosecond();
-				if(!track_test(m,resultbox)){
+				if(!cvtra.track_test(m,resultbox)){
 					t_i = true;
 					AINFO<<"lose one";
 					return true;
 				}
-				//AINFO<<"tracker cost time"<<Time::Now().ToNanosecond() - alog_start_time;
+				AINFO<<"tracker cost time"<<Time::Now().ToNanosecond() - alog_start_time;
 				uint64_t cal_finish = Time::Now().ToNanosecond();
 				cal_latency[0].push_back(cal_finish - alog_start_time);
 			}
--- cyber/detect_s/tracker_opencv.h
+++ cyber/detect_s/tracker_opencv.h
@@ -5,9 +5,14 @@
 #include <opencv2/core/ocl.hpp>
 using namespace cv;
 using namespace std;
+class CvTracker{
+	public:
 void tracker_create(string trackerType);
 void tracker_init(Mat& frame, Rect2d& bbox);
 bool track_test(Mat& frame, Rect2d& bbox);
+
+Ptr<Tracker> tracker;
+};
 //void imwrite_cv(string,Mat&);
 //Mat imread_cv(string);
 #endif
