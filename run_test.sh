7bef8db1508186daf83f4e0baffa545f2c394be6
sauron <1369130123qq@gmail.com>
2021-07-28 13:43:32 +0800

    delete some useless files

--- run_test.sh
+++
@@ -1,7 +0,0 @@
-kill -9 `ps -A|grep java |awk '{print $1}'`
-cyber_launch start cyber/AD_Middle_Test/cyber/intra_component/intra_shm.launch & ./bazel-bin/cyber/AD_Middle_Test/cyber/intra_no_component &
-sleep 150
-cyber_launch stop
-echo $1
-python3 /apollo/cyber/AD_Middle_Test/cyber/data/handle.py
-mv data/log/test/intra.txt data/log/test/$1_txt
